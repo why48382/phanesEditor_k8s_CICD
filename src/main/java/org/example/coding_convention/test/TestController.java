8ccde0358ef4f80c00195bae51614031b4863bb8
lee <why48382@gmail.com>
2025-09-17 17:29:48 +0900

    접속 테스트 하기

--- src/main/java/org/example/coding_convention/test/TestController.java
+++ src/main/java/org/example/coding_convention/test/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.ok("테스트02");
+        return ResponseEntity.ok("테스트03");
     }
 
     @GetMapping("/health")
